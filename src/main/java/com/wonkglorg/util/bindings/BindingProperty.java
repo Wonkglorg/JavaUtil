e2b69dcb2d7bcf2bab3df3fd78e9a5b82672fc6a
Wonkglorg <Wonkglorg.18@gmail.com>
2024-06-11 22:04:59 +0200

    Added new Pseudo Random Methods

---
+++ src/main/java/com/wonkglorg/util/bindings/BindingProperty.java
@@ -0,0 +1,8 @@
+package com.wonkglorg.util.bindings;
+
+/**
+ * Base class for all binding properties.
+ */
+public abstract class BindingProperty {
+    //todo orient after javafx.beans.property.Property
+}
