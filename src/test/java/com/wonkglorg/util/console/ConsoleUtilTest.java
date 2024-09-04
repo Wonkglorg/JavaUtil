b218fef9b9637c453cc1a96f7867d5b0c64786fe
Wonkglorg <Wonkglorg.18@gmail.com>
2024-09-04 17:45:34 +0200

    Minor fixes

--- src/test/java/com/wonkglorg/util/console/ConsoleUtilTest.java
+++ src/test/java/com/wonkglorg/util/console/ConsoleUtilTest.java
@@ -1,18 +1,27 @@
 package com.wonkglorg.util.console;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.Scanner;
+import com.wonkglorg.util.ip.IPv4;
+import com.wonkglorg.util.ip.IPv6;
 
 import static com.wonkglorg.util.console.ConsoleUtil.readInput;
 
 public class ConsoleUtilTest {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        int num = readInput(Integer.class);
+        int number = readInput(Integer.class, 0);
+        System.out.println("You entered: " + number);
 
+        System.out.println("Enter a char: ");
+        char character = readInput(Character.class, 'a');
+        System.out.println("You entered: " + character);
 
+        System.out.println("Enter an IPv4: ");
+        IPv4 ip = readInput(IPv4.class, IPv4.Min);
+        System.out.println("You entered: " + ip);
+
+        System.out.println("Enter a IPv6: ");
+        IPv6 ip6 = readInput(IPv6.class, IPv6.Min);
+        System.out.println("You entered: " + ip6);
     }
 }
