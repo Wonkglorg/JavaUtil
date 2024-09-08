7629ae0cdbf887aa3aa68052810274f49f98cd65
Wonkglorg <Wonkglorg.18@gmail.com>
2024-09-08 16:59:36 +0200

    Created DownloaderUtil

---
+++ src/main/java/com/wonkglorg/util/web/DownloaderUtil.java
@@ -0,0 +1,92 @@
+package com.wonkglorg.util.web;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+public class DownloaderUtil {
+    private static final Logger LOGGER = Logger.getLogger(DownloaderUtil.class.getName());
+
+    /**
+     * Gets a stream from the given URL.
+     *
+     * @param url The URL
+     * @return The stream or null if the stream could not be opened.
+     */
+    public InputStream getStream(String url, boolean extraInfo) {
+        try {
+            return URI.create(url).toURL().openStream();
+        } catch (IOException e) {
+            if (extraInfo) {
+                LOGGER.log(LOGGER.getLevel(), e.getMessage(), e);
+            }
+            return null;
+        }
+    }
+
+    /**
+     * Gets a stream from the given URL.
+     *
+     * @param url The URL
+     * @return The stream or null if the stream could not be opened.
+     */
+    public InputStream getStream(String url) {
+        return getStream(url, false);
+    }
+
+    /**
+     * Downloads a file from the given URL and saves it to the target path.
+     *
+     * @param url        The URL to download the file from.
+     * @param targetPath The path to save the downloaded file to.
+     * @param options    The options to use when copying the file.
+     * @return The downloaded / existing file or an empty optional if the download failed.
+     */
+    public Optional<File> downloadFile(String url, String targetPath, boolean extraInfo, StandardCopyOption... options) {
+        try (InputStream in = getStream(url)) {
+            if (in == null) {
+                return Optional.empty();
+            }
+            Path outputPath = Path.of(targetPath);
+            Files.copy(in, outputPath, options);
+            return Optional.of(outputPath.toFile());
+        } catch (IOException e) {
+            if (extraInfo) {
+                LOGGER.log(LOGGER.getLevel(), e.getMessage(), e);
+            }
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Downloads a file from the given URL and saves it to the target path.
+     *
+     * @param url        The URL to download the file from.
+     * @param targetPath The path to save the downloaded file to.
+     * @param options    The options to use when copying the file.
+     * @return The downloaded / existing file or an empty optional if the download failed.
+     */
+    public Optional<File> downloadFile(String url, String targetPath, StandardCopyOption... options) {
+        return downloadFile(url, targetPath, false, options);
+    }
+
+    /**
+     * Downloads a file from the given URL and saves it to the target path. If the file already exists nothing happens
+     *
+     * @param url        The URL to download the file from.
+     * @param targetPath The path to save the downloaded file to.
+     * @return The downloaded file / existing file if it already exists or an empty optional if the download failed.
+     */
+    public Optional<File> downloadFile(String url, String targetPath) {
+        if (Files.exists(Path.of(targetPath))) {
+            return Optional.of(new File(targetPath));
+        }
+        return downloadFile(url, targetPath, StandardCopyOption.REPLACE_EXISTING);
+    }
+}
