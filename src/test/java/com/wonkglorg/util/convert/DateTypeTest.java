3fc9898418fe6381aee7a686042fc2642cebc187
P10209 <Wonkglorg.18@gmail.com>
2024-09-25 13:56:47 +0200

    - Fixed Conversion Issue

--- src/test/java/com/wonkglorg/util/convert/DateTypeTest.java
+++ src/test/java/com/wonkglorg/util/convert/DateTypeTest.java
@@ -10,19 +10,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DateTypeTest {
 
 	@Test
-	void testStuff() {
-		String testTime = "124E 335ML 7D 7Y 4M 2W 5d 14h 21m 43s 333ms";
+	void convertBackAndFourth() {
+		String expectedTime = "124E 335ML 7D 7Y 4M 2W 5d 14h 21m 43s 333ms";
+		long expectedMillies = 3921031000333303333L;
 
+		long result = fromTimeString(expectedTime).toMilliseconds();
+		String revertedString = toTimeString().inputMillie(result).build();
+		System.out.println("Input time: " + expectedTime);
+		System.out.println("---------------------");
+		System.out.println("Millies result: " + result);
+		System.out.println("---------------------");
+		System.out.println("Reverse Conversion: " + revertedString);
+		assertEquals(expectedMillies, result);
+		assertEquals(expectedTime, revertedString);
 
-		long oldResult = ConverterDate.toMillis(testTime);
-		long result = fromTimeString(testTime).toMilliseconds();
-		System.out.println("Input time: " + testTime);
-		System.out.println("---------------------");
-		System.out.println("Old result: " + oldResult);
-		System.out.println("New result: " + result);
-		System.out.println("---------------------");
-		System.out.println("Old convert: " + toTimeString().inputMillie(oldResult).build());
-		System.out.println("New convert: " + toTimeString().inputMillie(result).build());
 
 	}
 
@@ -43,7 +44,7 @@ class DateTypeTest {
 		String timeFullName = toTimeString().inputSeconds(10).useFullName(true).build();
 		assertEquals("10 Seconds", timeFullName);
 		String timeFullNameNonCapital = toTimeString().inputSeconds(10).useFullName(false).build();
-		assertEquals("10 seconds", timeFullName);
+		assertEquals("10 seconds", timeFullNameNonCapital);
 
 	}
 
